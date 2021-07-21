b615456737b22ad56a9df41d9d2544fa8d094168
Benjamin VanCleave <cleaveapplications@gmail.com>
2021-07-20 22:42:28 -0400

    Added MainViewController to NavigationController via Storyboard and added a menu button to top left navigation

--- HopSkipDrive Test/Modules/Main/ViewController.swift
+++ HopSkipDrive Test/Modules/Main/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "My Rides"
+        createMenuButton()
     }
-
-
+    
+    private func createMenuButton() {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "menu")?.withTintColor(.white), for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        button.addTarget(self, action: #selector(openMenu), for: .touchUpInside)
+        let barButton = UIBarButtonItem(customView: button)
+        navigationItem.leftBarButtonItems = [barButton]
+    }
+    
+    @objc func openMenu() {
+        print("slide menu out")
+    }
 }
 
